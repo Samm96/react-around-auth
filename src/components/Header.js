fac7af25366d5bdab403399f4f3c161db8e12e6d
Samm96 <sihorsch96@gmail.com>
2022-06-25 19:28:54 -0400

    Add: SignIn & Login CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
-function Header() {
+function Header ({
+  logText,
+  userEmail,
+  loggedIn,
+  loggedOut
+
+}) {
   return (
       <header className="header">
         <img src={require("../images/logo.svg")} className="header__logo" alt="Around the U.S. Logo" />
+        <div className="header__container">
+          <p className="header__sign">logText</p>
+          <p className="header__email">userEmail</p>
+        </div>
       </header>
   );
 }
