376c9cee24515cc5c48f071dc44723e222719b4b
Samm96 <sihorsch96@gmail.com>
2022-06-28 13:43:23 -0400

    Add: BrowserRouter indexjs / Fix: changed headeremail into button headerjs

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ function Header ({
         <img src={require("../images/logo.svg")} className="header__logo" alt="Around the U.S. Logo" />
         <div className="header__container">
           <p className="header__sign">logText</p>
-          <p className={loggedIn ? "header__email" : "header__email_hidden"}>userEmail</p>
+          <button className={loggedIn ? "header__email" : "header__email_hidden"}>userEmail</button>
           <button className="menu-button" alt="Menu"></button>
         </div>
       </header>
