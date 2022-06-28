d54e4445f5018d60cd07155255efa5a625901d99
Samm96 <sihorsch96@gmail.com>
2022-06-27 22:08:09 -0400

    Add: Header useremail loggedin?

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ function Header ({
         <img src={require("../images/logo.svg")} className="header__logo" alt="Around the U.S. Logo" />
         <div className="header__container">
           <p className="header__sign">logText</p>
-          <p className="header__email">userEmail</p>
+          <p className={loggedIn ? "header__email" : "header__email_hidden"}>userEmail</p>
           <button className="menu-button" alt="Menu"></button>
         </div>
       </header>
